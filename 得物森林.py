1e0cd192800cfa55016bdf603663dd410a02f673
q7q7q7q7q7q7q7 <137859770+q7q7q7q7q7q7q7@users.noreply.github.com>
2023-08-16 10:30:26 +0800

    Update 得物森林.py

--- 得物森林.py
+++ 得物森林.py
@@ -462,7 +462,7 @@ class DeWu:
                 self.receive_task_reward(classify, task_id, task_type)  # 领取奖励
                 continue
 
-            if any(re.match(pattern, task_name) for pattern in ['.*逛逛.*', '浏览.*15s']):
+            if any(re.match(pattern, task_name) for pattern in ['.*逛逛.*', '浏览.*s']):
                 _json = {'taskId': task_id, 'taskType': task_type, 'btd': btd}
                 if self.task_commit_pre(_json):
                     print(f'等待16秒')
